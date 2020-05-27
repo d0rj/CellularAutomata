cb47daea6a7b085aad458a456e4f184e2c088360
d0rj_ <dima.balobin_barnaul@mail.ru>
2020-05-27 17:27:49 +0300

    Changed instrument buttons position, added fixed toolbar.

--- main.py
+++ main.py
@@ -12,29 +12,28 @@ CELL_COUNT = 60
 STEP_INTERVAL = 100
 default_rule = {'b': [3], 's': [2, 3]}
 FILETYPES = [('config files', '.cfg'), ('log files', '.log')]
+INSTRUMENTS_SIZE = 30
 
 
 def main():
     root = Tk()
     root.title('Celluar automat')
-    root.geometry('600x700')
-    f_top = Frame(root, width=(CELL_COUNT * CELL_SIZE), height=(CELL_COUNT * CELL_SIZE))
-    f_bot = Frame(root)
+    root.geometry('{}x{}'.format(CELL_COUNT * CELL_SIZE, (CELL_COUNT * CELL_SIZE) + INSTRUMENTS_SIZE))
+    f_map = Frame(root, width=(CELL_COUNT * CELL_SIZE), height=(CELL_COUNT * CELL_SIZE))
+    f_instruments = Frame(root)
 
-    play_icon = PhotoImage(file='.\images\play_icon.png').subsample(30, 30)
-    next_icon = PhotoImage(file='.\images\\next_icon.png').subsample(30, 30)
-    random_icon = PhotoImage(file='.\images\\random_icon.png').subsample(30, 30)
+    play_icon = PhotoImage(file='.\images\play_icon.png').subsample(INSTRUMENTS_SIZE, INSTRUMENTS_SIZE)
+    next_icon = PhotoImage(file='.\images\\next_icon.png').subsample(INSTRUMENTS_SIZE, INSTRUMENTS_SIZE)
+    random_icon = PhotoImage(file='.\images\\random_icon.png').subsample(INSTRUMENTS_SIZE, INSTRUMENTS_SIZE)
 
     cell_map = CellMap(CELL_COUNT, default_rule)
-    cell_map_widget = CellMapWidget(f_top, CELL_SIZE, STEP_INTERVAL, cell_map)
+    cell_map_widget = CellMapWidget(f_map, CELL_SIZE, STEP_INTERVAL, cell_map)
 
-    f_top.pack(fill=X, side=TOP)
-    f_bot.pack(fill=X, side=BOTTOM)
-    one_step = Button(f_bot, text='Step', command=cell_map_widget.step, image=next_icon, compound=LEFT)  
-    clear_button = Button(f_bot, text='Clear', command=cell_map_widget.on_clear)
-    simulate_button = Button(f_bot, text='Simulate', command=lambda: cell_map_widget.on_simulate(simulate_button), image=play_icon, compound=LEFT)
-    random_button = Button(f_bot, text='Random', command=cell_map_widget.on_randomize, image=random_icon, compound=LEFT)
-    log_button = Button(f_bot, text='Log', command=lambda: cell_map_widget.on_log(log_button))
+    one_step = Button(f_instruments, text='Step', command=cell_map_widget.step, image=next_icon, compound=LEFT, height=INSTRUMENTS_SIZE)  
+    clear_button = Button(f_instruments, text='Clear', command=cell_map_widget.on_clear, height=INSTRUMENTS_SIZE)
+    simulate_button = Button(f_instruments, text='Simulate', command=lambda: cell_map_widget.on_simulate(simulate_button), image=play_icon, compound=LEFT, height=INSTRUMENTS_SIZE)
+    random_button = Button(f_instruments, text='Random', command=cell_map_widget.on_randomize, image=random_icon, compound=LEFT, height=INSTRUMENTS_SIZE)
+    log_button = Button(f_instruments, text='Log', command=lambda: cell_map_widget.on_log(log_button), height=INSTRUMENTS_SIZE)
 
     main_menu = Menu()
 
@@ -70,11 +69,14 @@ def main():
     main_menu.add_cascade(label='File', menu=file_menu)
     main_menu.add_cascade(label='Config', menu=config_menu)
 
+    simulate_button.pack(side=LEFT)
     one_step.pack(side=LEFT)
-    clear_button.pack(side=RIGHT)
-    simulate_button.pack(side=BOTTOM)
-    random_button.pack(side=BOTTOM)
-    log_button.pack(side=BOTTOM)
+    clear_button.pack(side=LEFT)
+    random_button.pack(side=LEFT)
+    log_button.pack(side=LEFT)
+
+    f_map.pack(fill=X, side=BOTTOM)
+    f_instruments.pack(fill=X, side=TOP)
 
     root.config(menu=main_menu)
     root.mainloop()
